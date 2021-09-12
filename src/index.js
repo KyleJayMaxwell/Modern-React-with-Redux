769d32bdf06e3f1cf042bf0c075aeed2eee1605e
Kyle Maxwell <kyle.maxwell@Kyle-Maxwells-MacBook-Pro.local>
2021-09-12 16:47:27 -0700

    displaying content with functional components

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Create a react component
-
+const App = () => {
+    return <div>Hi There!</div>;
+};
 
 // Take react component and show it
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
